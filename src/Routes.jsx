f125402ffd2fb6c788661a58547edd94e65f4e57
Gaurav kumar <gk064577@gmail.com>
2025-05-21 23:25:39 +0530

    Addded logout functionality

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,29 +5,37 @@ import { NotFoundPage } from "@/pages/NotFound";
 import { SignupContainer } from "@/components/organism/Auth/SignupContainer";
 import { SigninContainer } from "@/components/organism/Auth/SigninContainer";
 import { ProtectedRoute } from "@/components/molecules/ProtectedRoute/ProtectedRoute";
-export const AppRoutes=()=>{
-    return (
-        <Routes>
-          <Route
-            path="/auth/signup"
-            element={
-              <Auth>
-                <SignupContainer />
-              </Auth>
-            }
-          />
-          <Route
-            path="/auth/signin"
-            element={
-              <Auth>
-                <SigninContainer />
-              </Auth>
-            }
-          />
 
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          {/* this is for not found page */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-    )
-}
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/auth/signup"
+        element={
+          <Auth>
+            <SignupContainer />
+          </Auth>
+        }
+      />
+      <Route
+        path="/auth/signin"
+        element={
+          <Auth>
+            <SigninContainer />
+          </Auth>
+        }
+      />
+
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      {/* this is for not found page */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
--- src/components/atom/UserButton/UserButton.jsx
+++ src/components/atom/UserButton/UserButton.jsx
@@ -8,31 +8,49 @@ import {
   DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu";
 import { useAuth } from "@/hooks/context/useAuth";
-import { LogOutIcon, Settings } from "lucide-react";
+import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 export const UserButton = () => {
-  const { auth } = useAuth();
+    const navigate=useNavigate();
+  const { auth,logout } = useAuth();
+
+  async function handleLogout() {
+    await logout();
+
+   toast.success("Logout  successful!", {
+     description: "You can now log in with your account.",
+     type: "success",
+
+   })
+   navigate("/auth/signin")
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="outline-none relative">
-        
-          <Avatar className="size-16 hover:opacity-85 hover:bg-amber-400 transition">
-            <AvatarImage src={auth?.user?.avatar} />
-            <AvatarFallback>
-              {auth?.user?.username[0].toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        
+        <Avatar className="size-12 hover:opacity-85 hover:bg-amber-400 transition">
+          <AvatarImage src={auth?.user?.avatar} />
+          <AvatarFallback>
+            {auth?.user?.username[0].toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-        <DropdownMenuItem><Settings/>settings</DropdownMenuItem>
-        <DropdownMenuItem><LogOutIcon></LogOutIcon>Logout</DropdownMenuItem>
+        <DropdownMenuItem> Profile</DropdownMenuItem>
+        
+        <DropdownMenuItem>
+          <SettingsIcon className="size-4 mr-2 h-10" />
+          settings
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={handleLogout}>
+          <LogOutIcon className="size-4 mr-2 h-10" />
+          Logout
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
